4ca46f49a97c0d64422e07595566bf34b6c69067
Gilberto <gilberto.tec@vivaldi.net>
2020-08-04 17:43:45 +0000

    colocando o basico da declaração de variveis

---
+++ basico/01 variaveis/basic.jl
@@ -0,0 +1,22 @@
+# Variaveis são declaradas e definidas 
+# pela atribuição
+#   Ao atribuir um valor vc determina o valo da
+#   variavel
+
+# String
+texto0 = "conteudo";
+# Int64
+numero0  = 1;
+# Float64
+numero1 = 1.0;
+# Float64
+numero2 = 1.0000000009;
+# Bool
+booleano0 = true;
+# tupla
+tupla0    = ("nome","gitt","fio");
+# Array
+vetor     = ["dd","dd","dd"];
+# dicionarios Set
+dicionario = Dict("nome" => "11","base" => "52");
+println(typeof(dicionario))
